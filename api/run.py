eafc2c89e367f918ae3e5438d369d214c9c2ea08
Jukelyn <viperkillergaming@gmail.com>
2025-04-08 19:56:42 -0400

    Removed debug from api

--- api/run.py
+++ api/run.py
@@ -14,4 +14,4 @@ CORS(app, origins=["http://localhost:3000",
 register_routes(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
