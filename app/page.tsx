6391b29e498b71b0195206ee49d6d6c981243f36
Jukelyn <viperkillergaming@gmail.com>
2025-04-09 02:27:09 -0400

    Removed styling from scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -142,10 +142,7 @@ export default function Home() {
                   <li key={badge.name}>
                     <ScrollArea className="rounded-md border py-2 max-h-16 whitespace-nowrap overflow-auto">
                       <code>{badge.markdown}</code>
-                      <ScrollBar
-                        orientation="horizontal"
-                        className="h-2 bg-gray-300"
-                      />
+                      <ScrollBar orientation="horizontal" />
                     </ScrollArea>
                   </li>
                 ))}
